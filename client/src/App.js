0bdfb8cb6a4732fed1a624d59fe17d49534092b1
devin-bielejec <devin.bieleje@gmail.com>
2019-09-06 11:37:28 -0400

    Got data to render to app from api

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import { runInThisContext } from 'vm';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: "Hi"
+      title: "Hi",
+      players: [{name: "Devin", country: "USA"}]
     }
   }
 
   componentDidMount() {
     axios.get("http://localhost:5000/api/players")
-    .then(res => console.log(res.data))
+    .then(res => {
+      console.log(res.data);
+      this.setState({players: res.data});
+    })
     .catch(err => console.log("Error:", err))
   }
 
@@ -21,6 +26,7 @@ class App extends Component {
     return(
       <div>
         <h1>{this.state.title}</h1>
+        <h2>{this.state.players[0].name}</h2>
       </div>
     )
     
